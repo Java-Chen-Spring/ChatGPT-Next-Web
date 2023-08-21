299ce482cbde6d29cc889764d1ad937ba893e57f
chenLIn <96414836+Java-Chen-Spring@users.noreply.github.com>
2023-08-21 15:33:45 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT(cl个人版)
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          拒绝白嫖,个人助手.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
